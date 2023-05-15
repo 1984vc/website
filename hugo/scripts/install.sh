e1858addedb6f187896a2343d73183eac466d659
Mark Percival <m@mdp.im>
2023-05-15 19:25:28 +0000

    Rework dir structure and add v2

--- hugo/scripts/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir -p bin
-cd bin
-wget https://github.com/gohugoio/hugo/releases/download/v0.111.3/hugo_0.111.3_linux-amd64.tar.gz
-tar xvf hugo_0.111.3_linux-amd64.tar.gz
-rm *.tar.gz
-cd ..
--- hugo/scripts/portfolio.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-curl -X GET -H "Authorization: Token $BASEROW_API_KEY" "https://api.baserow.io/api/database/rows/table/164534/?user_field_names=true" \
-    > data/portfolio.json
-
-# Example GOOGLE SHEET URL
-https://docs.google.com/spreadsheets/d/__ID__/gviz/tq?tqx=out:json&tq&gid=0
