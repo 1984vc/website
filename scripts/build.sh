6ca2b51402b34064138b7cad30dadcfa1395b8bb
Mark Percival <m@mdp.im>
2025-01-09 20:23:39 +0000

    Fix: simplify content

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,11 +10,6 @@ yarn test
 yarn build:hugo
 cd ..
 
-# Move our snapshot content to the content directory
-rm -rf ./content && mkdir ./content
-cp -R src-content/static/. ./content
-cp -R src-content/notion/. ./content
-
 # Build the project static root project
 echo "Building the project..."
 hugo
--- scripts/import-notion.sh
+++ scripts/import-notion.sh
@@ -6,4 +6,5 @@ npm install
 npx @1984vc/notion-tools export hextra -o ./tmp/content/docs --id 159829231d098087b96fd112a91c7430 --base-path docs
 npx @1984vc/notion-tools raw-json --id 155829231d0980779375ed60cc94a9de -o ./tmp/portfolio-raw.json
 node ./scripts/process-portfolio.mjs ./tmp/portfolio-raw.json ./data/portfolio.json
-node ./scripts/process-markdown.mjs ./tmp/content ./src-content/notion
+rm -rf ./content/notion
+node ./scripts/process-markdown.mjs ./tmp/content ./content/notion
