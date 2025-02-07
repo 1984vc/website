872d3a913e140f26bc297944f959eec5379d88c0
Mark Percival <m@mdp.im>
2025-02-07 14:23:10 +0000

    Chore: Update to pnpm everywhere

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,9 +5,9 @@ set -e
 # Build the the nextjs submodule
 echo "Building the nextjs submodule..."
 cd startup-finance
-yarn install --frozen-lockfile
-yarn test
-yarn build:hugo
+pnpm install
+pnpm test
+pnpm build:hugo
 cd ..
 
 # Build the project static root project
