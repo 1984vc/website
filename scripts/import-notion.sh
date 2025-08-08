4e72a1991c6483985a9b31f0245224172c8b73a7
Mark Percival <m@mdp.im>
2025-08-08 13:23:49 +0000

    Fix: PNPM install deps in workflows

--- scripts/import-notion.sh
+++ scripts/import-notion.sh
@@ -4,7 +4,6 @@ set -e
 # Pull in the content to the repo
 rm -rf ./content && mkdir -p ./content
 cd scripts/notion
-pnpm install
 pnpm run cli export hextra -o ../../content --id 159829231d098087b96fd112a91c7430 --assets-path ../../static/notion_assets --assets-path-base "/notion_assets" --base-url "https://1984.vc"
 pnpm run cli export json --id 155829231d0980779375ed60cc94a9de -o ../../data/portco.json
 cd ../..
