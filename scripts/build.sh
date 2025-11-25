a4374ed72fdbc936ef277965f6a03d404df7c15d
Mark Percival <m@mdp.im>
2025-11-25 16:51:55 -0500

    Feat: Move to startup finance, remove react app, update hextra

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,14 +2,6 @@
 
 set -e
 
-# Build the the nextjs submodule
-echo "Building the nextjs submodule..."
-cd startup-finance
-pnpm install
-pnpm test
-pnpm build:hugo
-cd ..
-
 echo "Building the project..."
 hugo
 
